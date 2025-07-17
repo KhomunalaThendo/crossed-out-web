fc6618e0ccf170f085b13e651a2760a633ecfeb7
Khomunala Thendo <thendocomz@gmail.com>
2025-07-17 15:39:50 +0200

    renderpage1

--- script.js
+++ script.js
@@ -68,3 +68,30 @@ const products = [
 ];
 
 //display products
+const renderProducts = () => {
+    const productSection = document.querySelector('.products');
+    //productSection.innerHTML = '';
+
+    products.forEach(product => {
+        const productDiv = document.createElement('div');
+        productDiv.classList.add('product');
+
+        productDiv.innerHTML = `
+         <img src="${product.image}" id="productPic" alt="">
+                <div class="productInfo">
+                    <p id="prodName">${product.name}</p>
+                    <p id="price">R ${product.price}</p>
+                </div>
+                <div class="ratingCart">
+                    <div class="rating">
+                        <img src="assets/icons/star.svg" alt="">
+                        <p id="rating">${product.rating}</p>
+                    </div>
+                    <div class="addCart">
+                        <img class="cartBtn" src="assets/icons/add-cart.png" alt="">
+                    </div>
+                </div>
+        `;
+        productSection.appendChild(productDIV);
+    });
+}
