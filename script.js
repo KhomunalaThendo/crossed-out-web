694f05312a6257fa9f60c4be2c2b989c2a42abad
Khomunala Thendo <thendocomz@gmail.com>
2025-07-17 15:11:27 +0200

    js init

---
+++ script.js
@@ -0,0 +1,70 @@
+const products = [
+    
+    {
+    id: 1,
+    name: 'Crossed Hoodie',
+    price: 350,
+    image: 'assets/images/hood-blk.jpg',
+    rating: 5
+    },
+    {
+    id: 2,
+    name: 'Crossed Hoodie',
+    price: 350,
+    image: 'assets/images/hood-blk.jpg',
+    rating: 5
+    },
+    {
+    id: 3,
+    name: 'Crossed Hoodie',
+    price: 350,
+    image: 'assets/images/hood-blk.jpg',
+    rating: 5
+    },
+    {
+    id: 4,
+    name: 'Crossed Hoodie',
+    price: 350,
+    image: 'assets/images/hood-blk.jpg',
+    rating: 5
+    },
+    {
+    id: 5,
+    name: 'Crossed Hoodie',
+    price: 350,
+    image: 'assets/images/hood-blk.jpg',
+    rating: 5
+    },
+    {
+    id: 6,
+    name: 'Crossed Hoodie',
+    price: 350,
+    image: 'assets/images/hood-blk.jpg',
+    rating: 5
+    },
+    {
+    id: 7,
+    name: 'Crossed Hoodie',
+    price: 350,
+    image: 'assets/images/hood-blk.jpg',
+    rating: 5
+    },
+    {
+    id: 8,
+    name: 'Crossed Hoodie',
+    price: 350,
+    image: 'assets/images/hood-blk.jpg',
+    rating: 5
+    },
+    {
+    id: 9,
+    name: 'Crossed Hoodie',
+    price: 350,
+    image: 'assets/images/hood-blk.jpg',
+    rating: 5
+    },
+
+
+];
+
+//display products
